88ed8589c816c7f07f22958620d2bb26b711e163
guofangchao <guofangchao@daling.com>
2020-01-29 10:40:16 +0800

    添加内容列表

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -8,17 +8,29 @@
             </el-carousel>
         </div>
         <div id="navs">
-            <a>物资寻求</a>
-            <a>物资供应</a>
-            <a>车辆供应</a>
-            <a>物资动态</a>
+            <router-link to="/foo">物资寻求</router-link>
+            <router-link to="/foo">物资供应</router-link>
+            <router-link to="/foo">车辆供应</router-link>
+            <router-link to="/foo">物资动态</router-link>
         </div>
         <div class="tabs">
-            全部
+            <ly-tab v-model="selectedId" change="tab_change" :items="items" :options="options"> </ly-tab>
         </div>
         <div class="list">
             <ul class="infinite-list" v-infinite-scroll="load" style="overflow:auto">
-                <li v-for="i in count" v-bind:key="i" class="infinite-list-item">{{ i }}</li>
+                <li v-for="i in count" v-bind:key="i" class="infinite-list-item">
+                    <div class="item1">
+                        <h3>医用口罩</h3>
+                        <p>供应量：1万件</p>
+                        <p>供货单位：某某医疗公司</p>
+                        <p>湖北省武汉市某某地点</p>
+                        <p>联系人：王某某</p>
+                        <div class="item1_foot">
+                            <span>电话：130000000000</span>
+                            <span class="date">2020-01-27</span>
+                        </div>
+                    </div>
+                </li>
             </ul>
         </div>
         <div class="line"></div>
@@ -34,13 +46,22 @@ export default {
     components: {},
     data() {
         return {
-            count: 0
+            menuKey: 'wz',
+            count: 0,
+            selectedId: 0,
+            items: [{ label: '首页' }, { label: '推荐' }, { label: 'Android' }],
+            options: {
+                activeColor: '#1d98bd'
+            }
         };
     },
     methods: {
         load() {
             if (this.count > 100) return;
             this.count += 2;
+        },
+        tab_change(item, index) {
+            console.log(item, index);
         }
     }
 };
@@ -60,8 +81,10 @@ export default {
 }
 #navs a {
     width: 2rem;
+    text-decoration: none;
+    color: #333;
 }
-.line{
+.line {
     height: 1.7rem;
 }
 .footer {
@@ -69,5 +92,18 @@ export default {
     bottom: 0.2rem;
     width: 100%;
     text-align: center;
+}
+.item1 {
+    border-bottom: solid 1px #ccc;
+    padding: 5px;
+}
+.item1_foot {
+    font-size: 0.4rem;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+.item1_foot .date {
+    font-size: .2rem;
 }
 </style>
