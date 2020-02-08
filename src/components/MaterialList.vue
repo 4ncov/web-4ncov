fd85d4d88c3f6af1f13133997d1b983008650fef
ran.meng <lrcry@vip.qq.com>
2020-02-08 21:35:34 +0800

    styles: material list item style

--- src/components/MaterialList.vue
+++ src/components/MaterialList.vue
@@ -5,16 +5,24 @@
                      :body-style="{ padding: '0px' }"
                      shadow="never">
                 <el-row class="material-item__section">
-                    <el-col :span="8">image</el-col>
-                    <el-col :span="16">
+                    <el-col :span="6">
+                        <el-image style="width: 100%; height: 100%"
+                                  :src="item.imageUrls && item.imageUrls[0]"
+                                  :fit="'fill'">
+                            <div slot="error" class="image-slot">
+                                暂无图片
+                            </div>
+                        </el-image>
+                    </el-col>
+                    <el-col :span="18" class="material-item__title">
                         <p><strong>{{item.material.name}}</strong></p>
-                        <p>执行标准: {{item.material.standard}}</p>
+                        <p class="material-item__title_standard">执行标准: {{item.material.standard}}</p>
                     </el-col>
                 </el-row>
-                <hr/>
+                <hr class="material-item__separator"/>
                 <div class="material-item__section">
                     <p><strong>{{item.organisationName}}</strong></p>
-                    <p>需：{{item.material.quantity}}件</p>
+                    <p class="material-item__section__quantity"><strong>需：{{item.material.quantity}}件</strong></p>
                     <p>收货地址: {{item.address}}</p>
                     <p>联系人: {{item.contactorName}}</p>
                     <p>联系方式: {{item.contactorPhone}}</p>
@@ -46,7 +54,24 @@
         border: 1px solid #aaa;
     }
 
+    .material-item__title {
+        padding-left: 0.4rem;
+    }
+
+    .material-item__title_standard {
+        color: #aaa;
+    }
+
+    .material-item__separator {
+        border-top: 1px solid #ddd;
+    }
+
     .material-item__section {
-        padding: 0.3rem 0.5rem;
+        padding: 0.5rem;
+    }
+
+    .material-item__section__quantity {
+        color: #f00;
+        margin-bottom: 0.3rem;
     }
 </style>
