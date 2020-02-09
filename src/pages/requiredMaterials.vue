0ca743f366dd2b114d36e897b189058e537d01ca
guofangchao <guofangchao@daling.com>
2020-02-09 15:06:17 +0800

    修改样式

--- src/pages/requiredMaterials.vue
+++ src/pages/requiredMaterials.vue
@@ -1,13 +1,14 @@
 <style scoped>
-.required-materials {
-  width: 100%;
+.topImg {
+    width: 100%;
 }
 </style>
 
 <template>
-  <div class="required-materials">
-    <PostMaterials :labels="labels" @submit="handleSubmit"></PostMaterials>
-  </div>
+    <div class="required-materials">
+        <img class="topImg" src="http://resource.guofangchao.com/4ncov/u213.png" />
+        <PostMaterials :labels="labels" @submit="handleSubmit"></PostMaterials>
+    </div>
 </template>
 
 <script>
@@ -15,48 +16,48 @@ import PostMaterials from '../components/PostMaterials.vue'
 import request from '../services/request'
 
 export default {
-  name: 'required-materials',
-  data() {
-    return {
-      // 如果required-materials和supplied-materials接口字段不一样才需要传
-      // formData: {
-      //   address: '',
-      //   comment: '',
-      //   contactorName: '',
-      //   contactorPhone: '',
-      //   hospitalName: '',
-      //   imageUrls: [],
-      //   materials: [
-      //     {
-      //       category: '',
-      //       name: '',
-      //       quantity: '',
-      //       standard: ''
-      //     }
-      //   ]
-      // },
-      labels: {
-        title: '物资寻求信息发布',
-        materials: '所需医疗物资信息',
-        contactor: '收货信息',
-        hospitalName: '发布机构名称',
-        contactorName: '联系人',
-        contactorPhone: '联系方式',
-        address: '收货地址',
-        uploadTip: '点击上传物资图片（可选）'
-      }
-    }
-  },
-  methods: {
-    handleSubmit(formData) {
-      console.log(formData)
-      request.post('/required-materials', formData).then(res => {
-        console.log(res)
-        // TODO：提交成功后的行为
-        alert('post required-materials success')
-      })
-    }
-  },
-  components: { PostMaterials }
+    name: 'required-materials',
+    data() {
+        return {
+            // 如果required-materials和supplied-materials接口字段不一样才需要传
+            // formData: {
+            //   address: '',
+            //   comment: '',
+            //   contactorName: '',
+            //   contactorPhone: '',
+            //   hospitalName: '',
+            //   imageUrls: [],
+            //   materials: [
+            //     {
+            //       category: '',
+            //       name: '',
+            //       quantity: '',
+            //       standard: ''
+            //     }
+            //   ]
+            // },
+            labels: {
+                title: '物资寻求信息发布',
+                materials: '所需医疗物资信息',
+                contactor: '收货信息',
+                hospitalName: '发布机构名称',
+                contactorName: '联系人',
+                contactorPhone: '联系方式',
+                address: '收货地址',
+                uploadTip: '点击上传物资图片（可选）'
+            }
+        }
+    },
+    methods: {
+        handleSubmit(formData) {
+            console.log(formData)
+            request.post('/required-materials', formData).then(res => {
+                console.log(res)
+                // TODO：提交成功后的行为
+                alert('post required-materials success')
+            })
+        }
+    },
+    components: { PostMaterials }
 }
 </script>
--- src/pages/suppliedMaterials.vue
+++ src/pages/suppliedMaterials.vue
@@ -1,13 +1,14 @@
 <style scoped>
-.supplied-materials {
-  width: 100%;
+.topImg {
+    width: 100%;
 }
 </style>
 
 <template>
-  <div class="supplied-materials">
-    <PostMaterials :labels="labels" @submit="handleSubmit"></PostMaterials>
-  </div>
+    <div class="supplied-materials">
+        <img class="topImg" src="http://resource.guofangchao.com/4ncov/u213.png" />
+        <PostMaterials :labels="labels" @submit="handleSubmit"></PostMaterials>
+    </div>
 </template>
 
 <script>
@@ -15,48 +16,48 @@ import PostMaterials from '../components/PostMaterials.vue'
 import request from '../services/request'
 
 export default {
-  name: 'supplied-materials',
-  data() {
-    return {
-      // 如果supplied-materials和required-materials接口字段不一样才需要传
-      // formData: {
-      //   address: '',
-      //   comment: '',
-      //   contactorName: '',
-      //   contactorPhone: '',
-      //   hospitalName: '',
-      //   imageUrls: [],
-      //   materials: [
-      //     {
-      //       category: '',
-      //       name: '',
-      //       quantity: '',
-      //       standard: ''
-      //     }
-      //   ]
-      // },
-      labels: {
-        title: '物资供应信息发布',
-        materials: '可供应医疗物资信息',
-        contactor: '供应商信息',
-        hospitalName: '供货机构名称',
-        contactorName: '联系人',
-        contactorPhone: '联系方式',
-        address: '发货地址',
-        uploadTip: '点击上传物资图片（可选）'
-      }
-    }
-  },
-  methods: {
-    handleSubmit(formData) {
-      console.log(formData)
-      request.post('/supplied-materials', formData).then(res => {
-        console.log(res)
-        // TODO：提交成功后的行为
-        alert('post supplied-materials success')
-      })
-    }
-  },
-  components: { PostMaterials }
+    name: 'supplied-materials',
+    data() {
+        return {
+            // 如果supplied-materials和required-materials接口字段不一样才需要传
+            // formData: {
+            //   address: '',
+            //   comment: '',
+            //   contactorName: '',
+            //   contactorPhone: '',
+            //   hospitalName: '',
+            //   imageUrls: [],
+            //   materials: [
+            //     {
+            //       category: '',
+            //       name: '',
+            //       quantity: '',
+            //       standard: ''
+            //     }
+            //   ]
+            // },
+            labels: {
+                title: '物资供应信息发布',
+                materials: '可供应医疗物资信息',
+                contactor: '供应商信息',
+                hospitalName: '供货机构名称',
+                contactorName: '联系人',
+                contactorPhone: '联系方式',
+                address: '发货地址',
+                uploadTip: '点击上传物资图片（可选）'
+            }
+        }
+    },
+    methods: {
+        handleSubmit(formData) {
+            console.log(formData)
+            request.post('/supplied-materials', formData).then(res => {
+                console.log(res)
+                // TODO：提交成功后的行为
+                alert('post supplied-materials success')
+            })
+        }
+    },
+    components: { PostMaterials }
 }
 </script>
